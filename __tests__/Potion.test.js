8b9c2d1fee727a68dfe16c8610a5e2820412c919
randy <baumer.randy@gmail.com>
2021-10-20 14:27:35 -0400

    added all player properties

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,9 +1,9 @@
-const Potion = require('../lib/Potion.js');
+const Potion = require("../lib/Potion.js");
 
-test('creates a health potion object', () => {
-    const potion = new Potion();
+test("creates a health potion object", () => {
+  const potion = new Potion();
 
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
 });
